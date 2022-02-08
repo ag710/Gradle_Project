1072cdf21f7654e368a3892bf1545750a0eadcbc
apoorv <apoorv.gautam@knoldus.com>
2022-02-08 21:06:14 +0530

    Initial commit

---
+++ module_1/src/main/java/App.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class App {
+    public static void main(String[] args)
+    {
+        System.out.println("Enter any Two Integers :");
+        Scanner sc= new Scanner(System.in);
+        Calculator calculator = new Calculator();
+        int a=sc.nextInt();
+        int b= sc.nextInt();
+        calculator.add(a,b);
+        calculator.sub(a,b);
+        calculator.prod(a,b);
+        calculator.div(a,b);
+    }
+}
